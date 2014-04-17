884c351f7cdb1f3789f4c955973ad0891d61c128
ispinoze <muratkirmizigul@gmail.com>
2014-04-17 14:41:43 +0300

    son

--- Audi.cs
+++ Audi.cs
@@ -7,7 +7,46 @@ namespace ornek_1
 {
     class Audi:IOrnek
     {
+        public string getMarka()
+        {
+            return "Audi";
+        }
 
+        private string _model = "A4";
+
+        public string model
+        {
+            get { return _model; }
+        }
+
+        private int _maxHiz;
+
+        public int maxHiz
+        {
+            get
+            {
+                return _maxHiz;
+            }
+            set
+            {
+                _maxHiz = value;
+            }
+        }
+
+        public int Gosterge(int deger)
+        {
+            return deger;
+        }
+        
+        public void BilgiYaz()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(" Marka : " +getMarka()+"\n");
+            Console.WriteLine(" Model : " + _model + "\n");
+            Console.WriteLine(" Max Hiz : " + _maxHiz + " km/h" + "\n");
+            Console.WriteLine(" Gösterge : " + _maxHiz + " km/h" + "\n");
+            Console.ReadLine();
+        }
 
     }
 }
